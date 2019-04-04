8c8963556a20075f87d55fd18e2ebdeee92e969f
jsdosa <jsdosa@gmail.com>
2019-04-03 22:35:22 -0400

    test owner
    Signed-off-by: jsdosa <jsdosa@gmail.com>

--- apps/examples/mediarecorder/mediarecorder_main.cpp
+++ apps/examples/mediarecorder/mediarecorder_main.cpp
@@ -346,6 +346,8 @@ public:
 
 	void printRecorderMenu()
 	{
+		std::cout << "testOWNERS" << std::endl;
+
 		std::cout << "========================================" << std::endl;
 		std::cout << "0.  RECORDER APP ON" << std::endl;
 		std::cout << "1.  RECORDER Start" << std::endl;
